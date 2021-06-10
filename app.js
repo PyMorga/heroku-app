8b1f57bbdf6ad703c75eaef0b32116112c506f6d
Marco El-Korashy <69629177+PyMorga@users.noreply.github.com>
2021-06-10 20:18:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const PORT = process.env.PORT || 3000;
 app.get("", (req, res) => {
-    res.send('Test');
+    res.send('index.html');
 });
 app.listen(PORT, () => {
     console.log('App upt port ${PORT}');
